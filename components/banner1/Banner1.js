b2303da122eab3177bd451ca32077dced80d0bf0
isiagi <isiagigeofrey0@gmail.com>
2023-08-03 15:45:11 +0300

    add new component

---
+++ components/banner1/Banner1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner1() {
+  return (
+    <div className="bg-[url('https://plus.unsplash.com/premium_photo-1679858780540-83cf9db66d55?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fG1vdmVyc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60')] h-screen">
+
+    </div>
+  )
+}
+
+export default Banner1
